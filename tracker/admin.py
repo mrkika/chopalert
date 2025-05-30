c5d2a1c5f638555d29a67c53a0de9ce153d72989
KikA-DyDx <edacocomputers@gmail.com>
2025-05-31 00:13:47 +0400

    Add superuser

--- tracker/admin.py
+++ tracker/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import FoodSpot  # make sure the model name matches
 
-# Register your models here.
+
+@admin.register(FoodSpot)
+class FoodSpotAdmin(admin.ModelAdmin):
+    list_display = ('food_type', 'description',
+                    'posted_at')  # customize as needed
+    search_fields = ('description', 'food_type')
+    list_filter = ('food_type', 'posted_at')
